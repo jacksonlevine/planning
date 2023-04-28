06168085216a51725d40e57bc0c625967938ed73
jacksonlevine <jack@netbug.us>
2023-04-28 13:02:15 -0700

    WIP: adding firebase key to react app

--- project/src/index.js
+++ project/src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: {process.env.REACT_APP_FIREBASE_API_KEY},
+  authDomain: "jacksongame-eb775.firebaseapp.com",
+  databaseURL: "https://jacksongame-eb775-default-rtdb.firebaseio.com",
+  projectId: "jacksongame-eb775",
+  storageBucket: "jacksongame-eb775.appspot.com",
+  messagingSenderId: "852977180992",
+  appId: "1:852977180992:web:ac8e4f04a47d35e96ddaa5",
+  measurementId: "G-5Y06CGYN2F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
