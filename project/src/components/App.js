275d1040e85049b264093396a093002fc98d3f61
jacksonlevine <jack@netbug.us>
2023-04-28 15:34:29 -0700

    update App to class component to hold state of page currently visible, etc

--- project/src/components/App.js
+++ project/src/components/App.js
@@ -1,9 +1,20 @@
-function App(props) {
-  return (
-    <div className="App">
-      <h1>{props.pid}</h1>
-    </div>
-  );
+import { Component } from "react";
+
+class App extends Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      pageVisible: "default"
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{props.pid}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
