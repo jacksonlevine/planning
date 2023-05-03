5fe90a9d1c31b56d06893f4265849e4b20b60532
jacksonlevine <jack@netbug.us>
2023-05-03 09:11:06 -0700

    update server.js

--- project/server/server.js
+++ project/server/server.js
@@ -12,16 +12,19 @@ const players = {};
 
 io.on('connection', (socket) => {
   console.log(`Client connected: ${socket.id}`);
-
+  players[socket.id] = {id: socket.id};
+  console.log(`Players connected now: ${Object.keys(players).length}`)
   socket.on('playerUpdate', (data) => {
     // handle incoming player update
     players[socket.id] = data;
     socket.broadcast.emit('playerUpdate', data);
+    
   });
 
   socket.on('disconnect', () => {
     console.log(`Client disconnected: ${socket.id}`);
     delete players[socket.id];
+    console.log(`Players connected now: ${Object.keys(players).length}`)
     socket.broadcast.emit('playerDisconnect', socket.id);
   });
 });
