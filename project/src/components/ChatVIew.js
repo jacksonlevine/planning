bf99f5866b20cb0c266c7540d4031aed5f0b78e7
jacksonlevine <jack@netbug.us>
2023-05-05 12:50:35 -0700

    update ChatView style

--- project/src/components/ChatVIew.js
+++ project/src/components/ChatVIew.js
@@ -6,9 +6,10 @@ export default function ChatView(props) {
             zIndex: "10",
             color: "white",
             position: "absolute",
-            left: "0",
-            top: "50%",
-            WebkitTextStroke: "0.5px black",
+            fontFamily: "monospace",
+            fontWeight: "bolder",
+            left: "17%",
+            top: "50%"
         }}>
             {props.chats.map((chat) =>
                 <p key={generateUUID()}>{chat.id}: {chat.message}</p>
