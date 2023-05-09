713be4a7aeb29992dd4e79b20822b4b8441420c4
Jackson <jack@netbug.us>
2023-05-09 13:21:49 -0700

    Remove suncalc from server

--- project/server/server.js
+++ project/server/server.js
@@ -1,6 +1,5 @@
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import SunCalc from 'suncalc';
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
@@ -43,24 +42,4 @@ httpServer.listen(3001, () => {
   console.log('Server listening on port 3001');
 });
 
-function sigmoid(x, k, x0) {
-  return 1 / (1 + Math.exp(-k * (x - x0)));
-}
 
-
-setInterval(() => {
-
-  const now = new Date();
-const times = SunCalc.getTimes(now, 38.7521, 121.2880);
-const sunriseTime = times.sunrise.getTime();
-const sunsetTime = times.sunset.getTime();
-const dayLength = sunsetTime - sunriseTime;
-  const centerTime = sunriseTime + (dayLength / 2);
-  const secondsFromCenter = now.getTime() - centerTime;
-  const brightness = sigmoid(secondsFromCenter/1000000, 0.002, centerTime/100000000000)*2;
-
-
-
-  io.emit('brightness', {brightness});
-  console.log(`Setting the brightness to ${brightness} at ${now.getHours() >= 12 ? now.getHours() - 12 : now.getHours()} ${now.getHours() >= 12 ? "PM" : "AM"}`);
-}, 60 * 1000);
