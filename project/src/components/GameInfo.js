9160caaf6a2079559ccb1a2a456147191aa6ee77
jacksonlevine <jack@netbug.us>
2023-05-08 23:21:51 -0700

    ui update 2

--- project/src/components/GameInfo.js
+++ project/src/components/GameInfo.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { generateUUID } from "three/src/math/MathUtils.js";
+import img1 from './../images/underconstruction.png';
 export default function GameInfo(props) {
     const [titleKey, setTitleKey] = useState();
     const [titleKey2, setTitleKey2] = useState();
@@ -17,13 +18,13 @@ export default function GameInfo(props) {
         case "signin":
         messageElement = 
         <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
-            <h1 key={titleKey2} className="fadey" style={{fontSize:"72px",
+            <h1 key={titleKey2} className="fadey" style={{
             position:"absolute",
             top:"25%"
         
         }}>MimosDono</h1>
 
-        <h1 key={titleKey} className="secondTitle" style={{fontSize:"72px",
+        <h1 key={titleKey} className="secondTitle" style={{
             position:"absolute",
             top:"25%"
         
@@ -32,6 +33,7 @@ export default function GameInfo(props) {
         <span className="red">m</span>
         <span className="red">o</span>
         <span className="red">s</span>Dono</h1>
+        <img className="construct" src={img1}alt="under construction"/>
         </div>
         break;
     }
