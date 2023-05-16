eefb352b5c584a55ea0016de489cc610ff38c053
jacksonlevine <jack@netbug.us>
2023-05-16 14:03:59 -0700

    finished chunk meshing code!

--- project/localclient/Chunk.cpp
+++ project/localclient/Chunk.cpp
@@ -47,6 +47,212 @@ void Chunk::rebuildMesh() {
 							{ (GLfloat)(tup.x),
 							(GLfloat)(tup.y),
 							(GLfloat)(tup.z) });
+						for (int i = 0; i < 6; i++)
+						{
+							this->colors.insert(this->colors.end(),
+								{
+									(GLfloat)1.0,
+									(GLfloat)0.0,
+									(GLfloat)0.0
+								});
+
+						}
+					}
+					if (this->gref->world.data.find(intTup(tup.x, tup.y, tup.z-1)) == this->gref->world.data.end())
+					{
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z) });
+
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z) });
+						for (int i = 0; i < 6; i++)
+						{
+							this->colors.insert(this->colors.end(),
+								{
+									(GLfloat)1.0,
+									(GLfloat)0.0,
+									(GLfloat)0.0
+								});
+
+						}
+					}
+					if (this->gref->world.data.find(intTup(tup.x + 1, tup.y, tup.z)) == this->gref->world.data.end())
+					{
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x+1),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z + 1) });
+
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z) });
+
+						for (int i = 0; i < 6; i++)
+						{
+							this->colors.insert(this->colors.end(),
+								{
+									(GLfloat)1.0,
+									(GLfloat)0.0,
+									(GLfloat)0.0
+								});
+
+						}
+					}
+					if (this->gref->world.data.find(intTup(tup.x, tup.y, tup.z + 1)) == this->gref->world.data.end())
+					{
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z + 1) });
+
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z + 1) });
+
+
+						for (int i = 0; i < 6; i++)
+						{
+							this->colors.insert(this->colors.end(),
+								{
+									(GLfloat)1.0,
+									(GLfloat)0.0,
+									(GLfloat)0.0
+								});
+
+						}
+					}
+					if (this->gref->world.data.find(intTup(tup.x, tup.y + 1, tup.z)) == this->gref->world.data.end())
+					{
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z + 1) });
+
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y + 1),
+							(GLfloat)(tup.z) });
+
+						for (int i = 0; i < 6; i++)
+						{
+							this->colors.insert(this->colors.end(),
+								{
+									(GLfloat)1.0,
+									(GLfloat)0.0,
+									(GLfloat)0.0
+								});
+
+						}
+					}
+					if (this->gref->world.data.find(intTup(tup.x, tup.y - 1, tup.z)) == this->gref->world.data.end())
+					{
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z + 1) });
+
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x + 1),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z + 1) });
+						this->vertices.insert(this->vertices.end(),
+							{ (GLfloat)(tup.x),
+							(GLfloat)(tup.y),
+							(GLfloat)(tup.z) });
+
+
+						for (int i = 0; i < 6; i++)
+						{
+							this->colors.insert(this->colors.end(),
+								{
+									(GLfloat)1.0,
+									(GLfloat)0.0,
+									(GLfloat)0.0
+								});
+
+						}
 					}
 				}
 			}
