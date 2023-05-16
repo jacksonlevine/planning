3b68268a49cb835756c5fe256134767753679a6d
jacksonlevine <jack@netbug.us>
2023-05-16 12:46:36 -0700

    give the world a game ref as well. trying to figure out how to break tuples into elements

--- project/localclient/World.cpp
+++ project/localclient/World.cpp
@@ -1,8 +1,20 @@
 #include "Game.h"
+#include "Perlin.h"
+
+const perlin p;
+
+World::World(Game* g) : gref(g) {
+
+}
+
 void World::generate() {
 
 }
 
-void World::generateOneChunk() {
+void World::generateOneChunk(std::tuple<int> coord) {
 
+	auto [x, y, z] = coord;
+	int realX = std::get<0>(coord) * this->gref->chunkWidth;
+	int realY = std::get<1>(coord) * this->gref->chunkWidth;
+	int realZ = std::get<2>(coord) * this->gref->chunkWidth;
 }
