ea69982bcc0255e982b50db85cec52b0ab119a45
jacksonlevine <jack@netbug.us>
2023-05-18 17:04:21 -0700

    update blocktypes

--- project/localclient/BlockTypes.h
+++ project/localclient/BlockTypes.h
@@ -1,4 +1,25 @@
 #pragma once
-class BlockTypes {
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp> 
+
+struct TextureFace {
+	glm::vec2 tl;
+	glm::vec2 bl;
+	glm::vec2 br;
+	glm::vec2 tr;
+};
+
+struct BlockType {
+public:
+	const bool allOneTexture;
+	const TextureFace top;
+	const TextureFace sides;
+	const TextureFace bottom;
 
 };
+
+extern const BlockType blockTypes[];
