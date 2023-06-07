2f5532a45ddf16b5cb897f7cf96b0c98758c8495
jacksonlevine <jack@netbug.us>
2023-06-07 13:52:50 -0700

    hud loose ends

--- project/simplechunksentt/Hud.hpp
+++ project/simplechunksentt/Hud.hpp
@@ -9,24 +9,12 @@ public:
 
 class HudView
 {
-private:
-	HudRect amalgam;
 public:
+	HudRect amalgam;
 	GLuint vbov;
 	GLuint vbouv;
 	bool dirty;
 	std::vector<HudRect> rects;
 	HudView();
-	HudRect getAmalgam();
+	void updateAmalgam();
 };
-
-HudRect HudView::getAmalgam()
-{
-	std::vector<GLfloat>().swap(this->amalgam.uvs);
-	std::vector<GLfloat>().swap(this->amalgam.verts);
-	for (HudRect& rect : this->rects)
-	{
-		this->amalgam.verts.insert(this->amalgam.verts.end(), rect.verts.begin(), rect.verts.end());
-		this->amalgam.uvs.insert(this->amalgam.uvs.end(), rect.uvs.begin(), rect.uvs.end());
-	}
-}
