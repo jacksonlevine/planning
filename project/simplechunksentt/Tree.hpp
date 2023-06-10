214194ceeb91e0ec780f95c4671b8e7b074a45fb
jacksonlevine <jack@netbug.us>
2023-06-09 20:30:56 -0700

    add textures and headers for functions for new objects

--- project/simplechunksentt/Tree.hpp
+++ project/simplechunksentt/Tree.hpp
@@ -12,10 +12,17 @@
 	class Rock {
 	public:
 		static Model getRockModel(float x, float y, float z);
+		static Model getSmallRockModel(float x, float y, float z);
 	};
 
 	class Plant {
 	public:
 		static Model getPricklyPearCactusModel(float x, float y, float z);
 		static Model getSaguaroCactusModel(float x, float y, float z);
+		static Model getCatTailsModel(float x, float y, float z);
+		static Model getTallGrassModel(float x, float y, float z);
+		static Model getShortGrassModel(float x, float y, float z);
+		static Model getDeadTallGrassModel(float x, float y, float z);
+		static Model getDeadShortGrassModel(float x, float y, float z);
+		static Model getBlackberryBushModel(float x, float y, float z);
 	};
